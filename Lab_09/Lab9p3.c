1367c5fab12c69362707b106aec890ea71d93177
Sutinan_Sriviset <sutinansriviset@gmail.com>
2026-09-25 11:10:03 +0700

    Finish Lab9p3.c

--- Lab_09/Lab9p3.c
+++ Lab_09/Lab9p3.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+
 void primecheck(int num)
 {
-    int Isprime = 0;
-    for (int i = 1; i < num; i++)
+    int Isprime = 1;
+
+    if (num < 2)
+        Isprime = 0;
+
+    for (int i = 2; i < num; i++)
     {
         if (num % i == 0)
         {
             Isprime = 0;
             break;
         }
-        else if (num % i != 0)
-        {
-            Isprime = 1;
-        }
     }
+
     if (Isprime == 1)
         printf("%4d", num);
 }
+
 int main()
 {
-    primecheck(2);
+    int num1, num2;
+    printf("Enter number [1] : ");
+    scanf("%d", &num1);
+    printf("Enter number [2] : ");
+    scanf("%d", &num2);
+    for (int i = num1; i <= num2; i++)
+    {
+        primecheck(i);
+    }
     return 0;
 }
